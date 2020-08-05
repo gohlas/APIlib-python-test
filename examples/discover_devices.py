b775999b2c44995c85a149384e3ca5a49c3ecae2
gohlas <50915149+gohlas@users.noreply.github.com>
2020-08-05 11:56:11 +0200

    Delete discover_devices.py

--- examples/discover_devices.py
+++
@@ -1,8 +0,0 @@
-"""Example to discover services on a device."""
-from sonyapilib.ssdp import SSDPDiscovery
-
-ip = "10.0.0.102"
-ssdp = SSDPDiscovery()
-services = ssdp.discover()
-for service in services:
-    print(service)
